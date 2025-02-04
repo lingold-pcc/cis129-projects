be9fa78d6a48c919d47963eb9a4792b9b5004b3b
lingold-pcc <lingold@mail.pima.edu>
2025-02-04 16:41:16 -0700

    Changed from printing hello world to a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+# Author: Liam Ingold
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('The sun went down in the desert.')
+print('Baby javelina followed her family around the neighborhood, snuffling for food')
+print('She found it: freshly planted flowers from our yard.')
+print('The ring camera catches the carnage')
+print('With full bellies, baby javelina and her family move on to the next yard.')
